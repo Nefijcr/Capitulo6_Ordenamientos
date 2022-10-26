4623f6457892baa05f6b476bc88c38eceab80dca
Nefi Jorge Cruzado Chucos <nefijcr@gmail.com>
2022-10-25 22:54:31 -0500

    ejercicios

---
+++ Company_Sistema_de_Ventas/src/Sistema_de_ventas/Sistema_de_Ventas.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ */
+package Sistema_de_ventas;
+
+/**
+ *
+ * @author nefij
+ */
+public class Sistema_de_Ventas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
